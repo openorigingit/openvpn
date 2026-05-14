e44c38a2544fca4971c2c1eae07390442de05905
Wesley Powell <wesley.powell@openorigin.industries>
2026-05-14 03:21:18 -0400

    Phase 6: test protected retransmit limits

--- tests/unit_tests/openvpn/test_provider_helper.c
+++ tests/unit_tests/openvpn/test_provider_helper.c
@@ -3451,12 +3451,17 @@ test_provider_helper_spawn_ikev2_unsupported_exchange(void **state)
         state_fd, state_initiator_spi, &state_material,
         PROVIDER_HELPER_IKEV2_EXCHANGE_CREATE_CHILD_SA, 2,
         PROVIDER_HELPER_IKEV2_NOTIFY_TEMPORARY_FAILURE, true);
+    for (uint32_t i = 0; i < PROVIDER_HELPER_DEFAULT_RETRANSMIT_LIMIT; ++i)
+    {
+        test_send_ikev2_encrypted_create_child_from(
+            state_fd, natt_port, state_initiator_spi, &state_material, true, 2);
+        test_recv_ikev2_encrypted_notify_exchange_response(
+            state_fd, state_initiator_spi, &state_material,
+            PROVIDER_HELPER_IKEV2_EXCHANGE_CREATE_CHILD_SA, 2,
+            PROVIDER_HELPER_IKEV2_NOTIFY_TEMPORARY_FAILURE, true);
+    }
     test_send_ikev2_encrypted_create_child_from(
         state_fd, natt_port, state_initiator_spi, &state_material, true, 2);
-    test_recv_ikev2_encrypted_notify_exchange_response(
-        state_fd, state_initiator_spi, &state_material,
-        PROVIDER_HELPER_IKEV2_EXCHANGE_CREATE_CHILD_SA, 2,
-        PROVIDER_HELPER_IKEV2_NOTIFY_TEMPORARY_FAILURE, true);
     test_send_ikev2_encrypted_protected_exchange_from(
         state_fd, natt_port, PROVIDER_HELPER_IKEV2_EXCHANGE_INFORMATIONAL,
         state_initiator_spi, &state_material, true, 4);
@@ -3502,8 +3507,8 @@ test_provider_helper_spawn_ikev2_unsupported_exchange(void **state)
     assert_int_equal(supervisor.state, PROVIDER_HELPER_STATE_READY);
     assert_int_equal(supervisor.last_rx_sequence, target_rx_sequence);
 #if defined(ENABLE_CRYPTO_OPENSSL)
-    assert_int_equal(supervisor.runtime_stats.datagrams_rx, 14);
-    assert_int_equal(supervisor.runtime_stats.datagrams_parsed, 14);
+    assert_int_equal(supervisor.runtime_stats.datagrams_rx, 19);
+    assert_int_equal(supervisor.runtime_stats.datagrams_parsed, 19);
     assert_int_equal(supervisor.runtime_stats.ike_exchange_unsupported, 3);
     assert_int_equal(supervisor.runtime_stats.ike_informational_empty_rx, 1);
     assert_int_equal(supervisor.runtime_stats.ike_informational_empty_response_tx,
@@ -3521,11 +3526,12 @@ test_provider_helper_spawn_ikev2_unsupported_exchange(void **state)
                      1);
     assert_int_equal(
         supervisor.runtime_stats.ike_create_child_temp_failure_failed, 0);
-    assert_int_equal(supervisor.runtime_stats.ike_exchange_replay_dropped, 1);
+    assert_int_equal(supervisor.runtime_stats.ike_exchange_replay_dropped, 2);
     assert_int_equal(supervisor.runtime_stats.ike_exchange_out_of_order_dropped,
                      1);
     assert_int_equal(supervisor.runtime_stats.ike_exchange_decrypt_failed, 1);
-    assert_int_equal(supervisor.runtime_stats.ike_exchange_retransmit_tx, 1);
+    assert_int_equal(supervisor.runtime_stats.ike_exchange_retransmit_tx,
+                     PROVIDER_HELPER_DEFAULT_RETRANSMIT_LIMIT);
     assert_int_equal(supervisor.runtime_stats.ike_exchange_retransmit_failed,
                      0);
     assert_int_equal(supervisor.runtime_stats.ike_sa_active, 0);
